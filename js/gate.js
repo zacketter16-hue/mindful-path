1d51d03fd55ba3499ab067d3961a06de47fd2482
zacketter16 <zacketter16@gmail.com>
2026-06-13 17:38:25 -0400

    Harden gate script error handling

--- js/gate.js
+++ js/gate.js
@@ -3,12 +3,18 @@
     window.$memberstackDom
       .getCurrentMember()
       .then(function (res) {
-        if (!res.data) {
-          window.$memberstackDom.openModal({ type: "LOGIN" }).then(function (res2) {
-            if (!res2 || !res2.data) {
+        if (!res || !res.data) {
+          try {
+            window.$memberstackDom.openModal("LOGIN").then(function (res2) {
+              if (!res2 || !res2.data) {
+                window.location.href = "/index.html";
+              }
+            }).catch(function () {
               window.location.href = "/index.html";
-            }
-          });
+            });
+          } catch (e) {
+            window.location.href = "/index.html";
+          }
         }
       })
       .catch(function () {
