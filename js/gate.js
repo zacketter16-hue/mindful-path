985bfa2ed9f4b4f30f40d1723a60a97d28e596c0
zacketter16 <zacketter16@gmail.com>
2026-06-13 17:35:37 -0400

    Make gate script wait for Memberstack to load and handle errors

--- js/gate.js
+++ js/gate.js
@@ -1,19 +1,28 @@
-document.addEventListener("DOMContentLoaded", function () {
+(function () {
   function runGate() {
-    window.$memberstackDom.getCurrentMember().then(function (res) {
-      if (!res.data) {
-        window.$memberstackDom.openModal("LOGIN").then(function (res2) {
-          if (!res2.data) {
-            window.location.href = "/index.html";
-          }
-        });
-      }
-    });
+    window.$memberstackDom
+      .getCurrentMember()
+      .then(function (res) {
+        if (!res.data) {
+          window.$memberstackDom.openModal({ type: "LOGIN" }).then(function (res2) {
+            if (!res2 || !res2.data) {
+              window.location.href = "/index.html";
+            }
+          });
+        }
+      })
+      .catch(function () {
+        window.location.href = "/index.html";
+      });
   }
 
-  if (window.$memberstackDom) {
-    runGate();
-  } else {
-    document.addEventListener("memberstack.ready", runGate);
+  function waitForMemberstack() {
+    if (window.$memberstackDom) {
+      runGate();
+    } else {
+      setTimeout(waitForMemberstack, 100);
+    }
   }
-});
+
+  waitForMemberstack();
+})();
