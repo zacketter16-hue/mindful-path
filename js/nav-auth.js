f3e17f6d16f312667292fd487e3dd3ecd2d2b3f8
zacketter16 <zacketter16@gmail.com>
2026-06-14 21:52:21 -0400

    Move mobile hamburger to left, show Log In/Join on right of mobile header

--- js/nav-auth.js
+++ js/nav-auth.js
@@ -9,9 +9,11 @@
       var loginLi = document.getElementById("nav-login");
       var joinLi = document.getElementById("nav-join");
       var logoutLi = document.getElementById("nav-logout");
+      var mobileAuth = document.querySelector(".nav-auth-mobile");
       if (member) {
         if (loginLi) loginLi.style.display = "none";
         if (joinLi) joinLi.style.display = "none";
+        if (mobileAuth) mobileAuth.style.display = "none";
         if (logoutLi) {
           logoutLi.style.display = "";
           logoutLi.addEventListener("click", function (e) {
